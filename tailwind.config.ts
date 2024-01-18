46ea69e3f9955f9851d483e1c6e7417387b14ef5
moeko-iwai <moepyxxxxxx@gmail.com>
2024-01-18 19:44:29 +0900

    update: カラーの定義をした

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: "#F3E04B",
+      "thin-primary": "#FAEE89",
+      secondary: "#64B090",
+      black: "#1A1A1C",
+      "deep-gray": "#D9D9D9",
+      gray: "#F0F0ED",
+      "thin-gray": "#F4F4F1",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      narrow: ["var(--font-narrow)", "sans-serif"],
+      noto: ["var(--font-noto)", "sans-serif"],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
