b1efe58371db45c1d8fd453dbc702db8afdf0890
moeko-iwai <moepyxxxxxx@gmail.com>
2024-02-03 18:38:37 +0900

    update: test ci failure

--- app/components/atoms/forms/FormCheckBox/FormCheckBox.stories.tsx
+++ app/components/atoms/forms/FormCheckBox/FormCheckBox.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useForm } from "react-hook-form";
-
+import { FormRadioGroup } from "../FormRadioGroup";
 import { FormCheckBox } from "./FormCheckBox";
 import { Button } from "../../Button";
 import { zodResolver } from "@hookform/resolvers/zod";
