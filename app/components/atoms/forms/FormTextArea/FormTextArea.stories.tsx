c251bd96b59a04d9d28201dfc29310eba7089af2
moeko-iwai <moepyxxxxxx@gmail.com>
2024-02-03 12:46:52 +0900

    add: TextAreaを追加した

---
+++ app/components/atoms/forms/FormTextArea/FormTextArea.stories.tsx
@@ -0,0 +1,144 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useForm } from "react-hook-form";
+
+import { FormTextArea } from "./FormTextArea";
+import { Button } from "../../Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+type Story = StoryObj<typeof FormTextArea>;
+
+const meta = {
+  title: "form/FormTextArea",
+  component: FormTextArea,
+  args: {
+    label: "気になることのメモ",
+  },
+} satisfies Meta<typeof FormTextArea>;
+
+export default meta;
+
+const BaseTemplate: Story["render"] = (args) => {
+  const schema = z.object({
+    condition: z.string(),
+  });
+
+  type FormSchemaType = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchemaType>({
+    defaultValues: {
+      condition: "",
+    },
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(console.log)} className="max-w-96">
+      <FormTextArea
+        {...args}
+        {...register("condition")}
+        errorMessage={errors.condition && errors.condition.message}
+      />
+      <Button
+        className="mt-2"
+        element={{
+          elementType: "button",
+          buttonType: "submit",
+        }}>
+        データ確認
+      </Button>
+    </form>
+  );
+};
+export const Base: Story = {
+  render: BaseTemplate,
+};
+
+const RequiredTemplate: Story["render"] = (args) => {
+  const schema = z.object({
+    condition: z.string().min(1, "必須項目です"),
+  });
+
+  type FormSchemaType = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchemaType>({
+    defaultValues: {
+      condition: "",
+    },
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(console.log)} className="max-w-96">
+      <FormTextArea
+        {...args}
+        {...register("condition")}
+        errorMessage={errors.condition && errors.condition.message}
+        required
+      />
+      <Button
+        className="mt-2"
+        element={{
+          elementType: "button",
+          buttonType: "submit",
+        }}>
+        データ確認
+      </Button>
+    </form>
+  );
+};
+export const Required: Story = {
+  render: RequiredTemplate,
+};
+
+const DisabledTemplate: Story["render"] = (args) => {
+  const schema = z.object({
+    condition: z.string().min(1, "必須項目です"),
+  });
+
+  type FormSchemaType = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchemaType>({
+    defaultValues: {
+      condition: "とっても元気です！",
+    },
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(console.log)} className="max-w-96">
+      <FormTextArea
+        {...args}
+        {...register("condition")}
+        errorMessage={errors.condition && errors.condition.message}
+        disabled
+      />
+      <Button
+        className="mt-2"
+        element={{
+          elementType: "button",
+          buttonType: "submit",
+        }}>
+        データ確認
+      </Button>
+    </form>
+  );
+};
+export const Disabled: Story = {
+  render: DisabledTemplate,
+};
---
+++ app/components/atoms/forms/FormTextArea/FormTextArea.tsx
@@ -0,0 +1,52 @@
+import { forwardRef, useId } from "react";
+import { Typography } from "../../Typography";
+import { InputProps } from "../FormCheckBox";
+import { tv } from "tailwind-variants";
+
+const textAreaStyle = tv({
+  base: "mt-2 p-1 border border-solid border-deep-gray rounded w-full",
+  variants: {
+    disabled: {
+      true: "text-deep-gray",
+    },
+  },
+});
+
+type Props = InputProps & {
+  rows?: number;
+};
+
+// eslint-disable-next-line react/display-name
+export const FormTextArea = forwardRef<HTMLTextAreaElement, Props>(
+  ({ label, required, disabled, errorMessage, rows = 5, ...restArgs }, ref) => {
+    const uniqueId = useId();
+    return (
+      <div className="my-4">
+        <label htmlFor={uniqueId} className="flex items-center">
+          <Typography>{label}</Typography>
+          {required && <Typography size="small">（入力必須です）</Typography>}
+          {disabled && <Typography size="small">（入力できません）</Typography>}
+        </label>
+        <textarea
+          {...restArgs}
+          ref={ref}
+          id={uniqueId}
+          aria-invalid={errorMessage ? true : false}
+          aria-describedby={`${uniqueId}-error`}
+          className={`${textAreaStyle({ disabled })}`}
+          disabled={disabled}
+          rows={rows}
+        />
+        {errorMessage && (
+          <Typography
+            color="error"
+            size="small"
+            id={`${uniqueId}-error`}
+            className="mt-2">
+            {errorMessage}
+          </Typography>
+        )}
+      </div>
+    );
+  }
+);
