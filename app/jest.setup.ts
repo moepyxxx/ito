c4b0f2c02b41e5d2d2db5b6709c0fe1fa58beda1
moeko-iwai <moepyxxxxxx@gmail.com>
2024-02-08 01:48:08 +0900

    update: vitest replace

--- app/jest.setup.ts
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-
-// see: https://github.com/kasir-barati/my-nextjs-journey
-global.console = {
-  ...console,
-  log: jest.fn(),
-  info: jest.fn(),
-  error: jest.fn(),
-} as unknown as Console;
---
+++ app/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+  },
+  resolve: {
+    // see: https://qiita.com/Garyo99/items/efacbacd5d8d3cf1848a
+    alias: [{ find: "@", replacement: resolve(__dirname, "./") }],
+  },
+});
