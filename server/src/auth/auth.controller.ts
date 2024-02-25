039642b194b14b2fce1d291e22e77a080f345e59
moeko-iwai <moepyxxxxxx@gmail.com>
2024-02-25 14:00:15 +0900

    update: handling server error

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { SupabaseService } from 'src/supabase.service';
 import { AuthUser } from './models/AuthUser.model';
 import { InputAuthUser } from './models/InputAuthUser.model';
+import { AuthApiError } from '@supabase/supabase-js';
+
+const UNAUTHORIZED_MESSAGE = 'ユーザーIDまたはパスワードが違います';
+const USER_DUPLICATED_MESSAGE = 'ユーザーは既に登録されています';
 
 @Controller('api/auth')
 export class AuthController {
@@ -11,13 +22,27 @@ export class AuthController {
   @HttpCode(204)
   async signup(@Body('user') user: InputAuthUser): Promise<AuthUser> {
     const { email, password } = user;
-    const { data } = await this.supabaseService.supabase.auth.signUp({
+    const { data, error } = await this.supabaseService.supabase.auth.signUp({
       email,
       password,
       options: {
         emailRedirectTo: 'http://localhost:3000/p',
       },
     });
+
+    if (error && data.user == null) {
+      if (error.message === 'User already registered') {
+        throw new HttpException(
+          USER_DUPLICATED_MESSAGE,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        'サーバーエラーが発生しました',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
     return new AuthUser({
       email: data?.user?.email,
       id: data?.user?.id,
@@ -29,16 +54,29 @@ export class AuthController {
   @Post('signin')
   async signin(@Body('user') user: InputAuthUser): Promise<AuthUser> {
     const { email, password } = user;
-    const { data } =
+    const { data, error } =
       await this.supabaseService.supabase.auth.signInWithPassword({
         email,
         password,
       });
+
+    if (error && data.user == null) {
+      if (error instanceof AuthApiError) {
+        console.log('api error dayo');
+        throw new HttpException(UNAUTHORIZED_MESSAGE, HttpStatus.UNAUTHORIZED);
+      } else {
+        throw new HttpException(
+          'サーバーエラーが発生しました',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+
     return new AuthUser({
-      email: data?.user?.email,
-      id: data?.user?.id,
-      refresh_token: data?.session?.refresh_token,
-      access_token: data?.session?.access_token,
+      email: data.user.email,
+      id: data.user.id,
+      refresh_token: data.session.refresh_token,
+      access_token: data.session.access_token,
     });
   }
 }
