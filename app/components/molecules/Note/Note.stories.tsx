02cedb5912b2237c1bdb4b3bf88ab52af1419ca7
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-02 14:03:32 +0900

    add: note to notitle pattern

--- app/components/molecules/Note/Note.stories.tsx
+++ app/components/molecules/Note/Note.stories.tsx
@@ -14,7 +14,7 @@ const meta = {
   ],
   args: {
     title: "目標について",
-    content: (
+    children: (
       <Typography size="small">
         itoは毎日の鳥さんの健康状態の”変化”に気づくことを目的としており、基本的には記録された体重・食べたご飯の量やお水の量に関して、毎日の変化を通知します。しかし、すでにお医者さんをはじめとした専門の方により目指すべき標準目標の約束がある場合、毎日の変化について標準情報と比べた結果をレポートとして作成します。基本的にすでにお医者さんにかかった鳥さんのための機能であり、この目標はあなたの鳥さんを直接見た専門の方からのアドバイスがある場合のみ設定することをおすすめしています。
       </Typography>
@@ -26,3 +26,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {};
+
+export const NoTitle: Story = {
+  args: {
+    title: undefined,
+  },
+};
--- app/components/molecules/Note/Note.tsx
+++ app/components/molecules/Note/Note.tsx
@@ -1,14 +1,14 @@
 import { Typography } from "@/components/atoms/Typography";
 
 type Props = {
-  title: string;
-  content: React.ReactNode;
+  title?: string;
+  children: React.ReactNode;
 };
-export const Note: React.FC<Props> = ({ title, content }) => {
+export const Note: React.FC<Props> = ({ title, children }) => {
   return (
     <div className="bg-gray p-4 rounded-xl">
-      <Typography bold>{title}</Typography>
-      {content}
+      {title && <Typography bold>{title}</Typography>}
+      {children}
     </div>
   );
 };
