c4d57901b915c185a871a963a4bd383fb5184b72
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-06 20:00:08 +0900

    add: 3つのstepperをつなげた

--- app/components/layouts/StepperLayout/StepperContent.tsx
+++ app/components/layouts/StepperLayout/StepperContent.tsx
@@ -2,9 +2,5 @@ type Props = {
   children: React.ReactNode;
 };
 export const StepperContent = ({ children }: Props) => {
-  return (
-    <div className="mt-14 border-solid border-black border rounded text-center">
-      {children}
-    </div>
-  );
+  return <div className="mt-14">{children}</div>;
 };
--- app/features/torisan/components/RegisterTorisanForm/RegisterTorisanForm.tsx
+++ app/features/torisan/components/RegisterTorisanForm/RegisterTorisanForm.tsx
@@ -1,60 +1,85 @@
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { FormTextBox } from "@/components/atoms/forms/FormTextBox";
-import { Button } from "@/components/atoms/Button";
+import { useStepper } from "@/components/layouts/StepperLayout/useStepper";
+import {
+  StepperContent,
+  StepperLabels,
+} from "@/components/layouts/StepperLayout";
+import {
+  FormSubmitType as TorisanBasic,
+  StepperFormBasic,
+} from "./StepperFormBasic";
+import {
+  FormSubmitType as TorisanObjective,
+  StepperFormObjective,
+} from "./StepperFormObjective";
+import {
+  FormSubmitType as TorisanFood,
+  StepperFormFood,
+} from "./StepperFormFood";
+import { useState } from "react";
+import { StepperFormConfirm } from "./StepperFormConfirm";
+
+const StepLabelPairs = [
+  { step: "basic", label: "基本" },
+  { step: "objective", label: "目標" },
+  { step: "food", label: "ごはん" },
+  { step: "confirm", label: "確認" },
+];
 
 type Props = {
   // TODO: 型を入れる
   onSubmit: (torisan: any) => void;
 };
-export const RegisterTorisanForm: React.FC<Props> = () => {
-  const schema = z.object({
-    name: z.string().min(1),
-    nickname: z.string().min(1),
-    specie_id: z.coerce.number().nullable(),
-    gender: z.coerce
-      .number()
-      .refine((value) => Object.values(Object).includes(value))
-      .nullable(),
-    birth_date: z.string().datetime().nullable(),
-  });
-
-  type FormSchemaType = z.infer<typeof schema>;
-
+export const RegisterTorisanForm: React.FC<Props> = ({ onSubmit }) => {
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormSchemaType>({
-    defaultValues: {
-      name: "",
-      nickname: "",
-      specie_id: null,
-      gender: null,
-      birth_date: null,
-    },
-    mode: "onChange",
-    resolver: zodResolver(schema),
-  });
+    currentStep,
+    currentStepIndex,
+    onSpecificStep,
+    renderStepperActions,
+  } = useStepper(StepLabelPairs, 0, "送信する", () => onSubmit("hoge"));
+
+  const [torisanBasic, setTorisanBasic] = useState<TorisanBasic | null>(null);
+  const [torisanObjective, setTorisanObjective] =
+    useState<TorisanObjective | null>(null);
+  const [torisanFood, setTorisanFood] = useState<TorisanFood | null>(null);
+
+  const contents = () => {
+    switch (currentStep) {
+      case "basic":
+        return (
+          <StepperFormBasic
+            renderStepperActions={renderStepperActions}
+            onSubmit={(data) => setTorisanBasic(data)}
+          />
+        );
+      case "objective":
+        return (
+          <StepperFormObjective
+            renderStepperActions={renderStepperActions}
+            onSubmit={(data) => setTorisanObjective(data)}
+          />
+        );
+      case "food":
+        return (
+          <StepperFormFood
+            renderStepperActions={renderStepperActions}
+            onSubmit={(data) => setTorisanFood(data)}
+          />
+        );
+      case "confirm":
+        return (
+          <StepperFormConfirm renderStepperActions={renderStepperActions} />
+        );
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
-      <FormTextBox
-        label="名前"
-        inputType="text"
-        {...register("name")}
-        required
-        errorMessage={errors.name && errors.name.message}
+    <>
+      <StepperLabels
+        stepLabelPairs={StepLabelPairs}
+        currentStepIndex={currentStepIndex}
+        onClickSpecificStep={onSpecificStep}
       />
-      <Button
-        className="mt-2"
-        element={{
-          elementType: "button",
-          buttonType: "submit",
-        }}>
-        データ確認
-      </Button>
-    </form>
+      <StepperContent>{contents()}</StepperContent>
+    </>
   );
 };
