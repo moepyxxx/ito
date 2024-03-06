998ece539b1b22e20962e0ba5f36439abc55740d
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-06 22:05:40 +0900

    refactor: form textbox story template

--- app/components/atoms/forms/FormTextBox/FormTextBox.stories.tsx
+++ app/components/atoms/forms/FormTextBox/FormTextBox.stories.tsx
@@ -1,17 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { useForm } from "react-hook-form";
 
 import { FormTextBox } from "./FormTextBox";
-import { Button } from "../../Button";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import {
-  createTextBoxNumberSchema,
-  createTextBoxSchema,
-} from "./createTextBoxSchema";
-import { getErrorMessage } from "@/utils";
 
-type Story = StoryObj<typeof FormTextBox>;
+import {
+  BaseTemplate,
+  DisabledTemplate,
+  EditTemplate,
+  RequiredTemplate,
+} from "./StoryTemplate";
+
+export type Story = StoryObj<typeof FormTextBox>;
 
 const meta = {
   title: "forms/FormTextBox",
@@ -24,181 +22,18 @@ const meta = {
 
 export default meta;
 
-const BaseTemplate: Story["render"] = (args: any) => {
-  const schema = z.object({
-    condition: createTextBoxSchema({
-      required: false,
-    }),
-  });
-
-  type FormSchemaType = z.infer<typeof schema>;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormSchemaType>({
-    defaultValues: {
-      condition: "",
-    },
-    mode: "onChange",
-    resolver: zodResolver(schema),
-  });
-
-  return (
-    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
-      <FormTextBox
-        {...args}
-        {...register("condition")}
-        errorMessage={errors.condition && errors.condition.message}
-      />
-      <Button
-        className="mt-2"
-        element={{
-          elementType: "button",
-          buttonType: "submit",
-        }}>
-        データ確認
-      </Button>
-    </form>
-  );
-};
 export const Base: Story = {
   render: BaseTemplate,
 };
 
-const RequiredTemplate: Story["render"] = (args: any) => {
-  const schema = z.object({
-    condition: createTextBoxSchema({
-      required: true,
-      requiredMessage: getErrorMessage({ type: "required" }),
-    }),
-  });
-
-  type FormSchemaType = z.infer<typeof schema>;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormSchemaType>({
-    defaultValues: {
-      condition: "",
-    },
-    mode: "onChange",
-    resolver: zodResolver(schema),
-  });
-
-  return (
-    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
-      <FormTextBox
-        {...args}
-        {...register("condition")}
-        errorMessage={errors.condition && errors.condition.message}
-        required
-      />
-      <Button
-        className="mt-2"
-        element={{
-          elementType: "button",
-          buttonType: "submit",
-        }}>
-        データ確認
-      </Button>
-    </form>
-  );
-};
 export const Required: Story = {
   render: RequiredTemplate,
 };
 
-const DisabledTemplate: Story["render"] = (args: any) => {
-  const schema = z.object({
-    condition: createTextBoxSchema({
-      required: false,
-    }),
-  });
-
-  type FormSchemaType = z.infer<typeof schema>;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormSchemaType>({
-    defaultValues: {
-      condition: "とっても元気です！",
-    },
-    mode: "onChange",
-    resolver: zodResolver(schema),
-  });
-
-  return (
-    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
-      <FormTextBox
-        {...args}
-        {...register("condition")}
-        errorMessage={errors.condition && errors.condition.message}
-        disabled
-      />
-      <Button
-        className="mt-2"
-        element={{
-          elementType: "button",
-          buttonType: "submit",
-        }}>
-        データ確認
-      </Button>
-    </form>
-  );
-};
 export const Disabled: Story = {
   render: DisabledTemplate,
 };
 
-const NumberTemplate: Story["render"] = (args: any) => {
-  const schema = z.object({
-    body_weight: createTextBoxNumberSchema({
-      required: false,
-    }),
-  });
-
-  type FormSchemaType = z.infer<typeof schema>;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<{ body_weight: number | null }, any, FormSchemaType>({
-    defaultValues: {
-      body_weight: null,
-    },
-    mode: "onChange",
-    resolver: zodResolver(schema),
-  });
-
-  return (
-    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
-      <FormTextBox
-        {...args}
-        {...register("body_weight")}
-        errorMessage={errors.body_weight && errors.body_weight.message}
-      />
-      <Button
-        className="mt-2"
-        element={{
-          elementType: "button",
-          buttonType: "submit",
-        }}>
-        データ確認
-      </Button>
-    </form>
-  );
-};
-export const Number: Story = {
-  render: NumberTemplate,
-  args: {
-    label: "体重（g）",
-    inputType: "number",
-  },
+export const Edit: Story = {
+  render: EditTemplate,
 };
---
+++ app/components/atoms/forms/FormTextBox/StoryTemplate.tsx
@@ -0,0 +1,110 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "../../Button";
+import { Story } from "./FormTextBox.stories";
+import { getErrorMessage } from "@/utils";
+import { FormTextBox } from "./FormTextBox";
+import { createTextBoxSchema } from "./createTextBoxSchema";
+
+export const useStoryTemplate = (
+  options: {
+    defaultValue: string;
+    required: boolean;
+    disabled: boolean;
+  },
+  args: any
+) => {
+  const { defaultValue, required, disabled } = options;
+
+  const schema = z.object({
+    condition: createTextBoxSchema({
+      required,
+      requiredMessage: getErrorMessage({ type: "required" }),
+    }),
+  });
+
+  type FormSchemaType = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchemaType>({
+    defaultValues: {
+      condition: defaultValue,
+    },
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
+
+  const render = (
+    <form onSubmit={handleSubmit(console.warn)} className="max-w-96">
+      <FormTextBox
+        {...args}
+        {...register("condition")}
+        errorMessage={errors.condition && errors.condition.message}
+        required={required}
+        disabled={disabled}
+      />
+      <Button
+        className="mt-2"
+        element={{
+          elementType: "button",
+          buttonType: "submit",
+        }}>
+        データ確認
+      </Button>
+    </form>
+  );
+  return render;
+};
+
+export const BaseTemplate: Story["render"] = (args: any) => {
+  const render = useStoryTemplate(
+    {
+      defaultValue: "",
+      required: false,
+      disabled: false,
+    },
+    args
+  );
+  return render;
+};
+
+export const RequiredTemplate: Story["render"] = (args: any) => {
+  const render = useStoryTemplate(
+    {
+      defaultValue: "null",
+      required: true,
+      disabled: false,
+    },
+    args
+  );
+  return render;
+};
+
+export const DisabledTemplate: Story["render"] = (args: any) => {
+  const render = useStoryTemplate(
+    {
+      defaultValue: "体調は良好です。元気に部屋の中を飛び回っています！",
+      required: false,
+      disabled: true,
+    },
+    args
+  );
+  return render;
+};
+
+export const EditTemplate: Story["render"] = (args: any) => {
+  const render = useStoryTemplate(
+    {
+      defaultValue: "体調は良好です。元気に部屋の中を飛び回っています！",
+      required: false,
+      disabled: false,
+    },
+    args
+  );
+  return render;
+};
