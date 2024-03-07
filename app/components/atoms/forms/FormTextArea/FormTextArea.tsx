542a4073c6ca72c93061b7c3baca96552e3a0c4c
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-07 21:11:08 +0900

    update: FormTextAreaのスタイルを変更

--- app/components/atoms/forms/FormTextArea/FormTextArea.tsx
+++ app/components/atoms/forms/FormTextArea/FormTextArea.tsx
@@ -4,7 +4,7 @@ import { InputProps } from "../FormCheckBox";
 import { tv } from "tailwind-variants";
 
 const textAreaStyle = tv({
-  base: "mt-1 p-1 border border-solid border-deep-gray rounded max-w-96",
+  base: "mt-1 p-1 border border-solid border-deep-gray rounded max-w-96 w-full",
   variants: {
     disabled: {
       true: "text-deep-gray",
