f2261927db7b66e6698c3f86e6aed544a245867d
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-19 20:11:23 +0900

    refactor: repositoryを追加した

---
+++ server/src/torisans/torisans.repository.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@nestjs/common';
+import type {
+  torisan as TorisanType,
+  torisan_objective as TorisanObjectiveType,
+  torisan_staple_food as TorisanStapleFoodType,
+} from '../../prisma/generated';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class TorisansRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async createTorisan(
+    userId: string,
+    torisan: Omit<TorisanType, 'id' | 'created_at' | 'updated_at' | 'user_id'>,
+  ): Promise<TorisanType> {
+    return await this.prisma.getRlsClient(userId).torisan.create({
+      data: {
+        name: torisan.name,
+        nickname: torisan.nickname,
+        specie_type: torisan.specie_type,
+        birth_date: torisan.birth_date,
+        gender_type: torisan.gender_type,
+        stage_type: torisan.stage_type,
+        user_id: userId,
+      },
+    });
+  }
+
+  async createTorisanObjective(
+    userId: string,
+    torisanId: number,
+    torisan: Omit<
+      TorisanObjectiveType,
+      'id' | 'created_at' | 'updated_at' | 'user_id' | 'torisan_id'
+    >,
+  ): Promise<TorisanObjectiveType> {
+    return await this.prisma.getRlsClient(userId).torisan_objective.create({
+      data: {
+        torisan_id: torisanId,
+        body_weight: torisan.body_weight,
+        amount_of_water: torisan.amount_of_water,
+        amount_of_staple_food: torisan.amount_of_staple_food,
+        user_id: userId,
+      },
+    });
+  }
+
+  async createTorisanFood(
+    userId: string,
+    torisanId: number,
+    torisan: Omit<
+      TorisanStapleFoodType,
+      'id' | 'created_at' | 'updated_at' | 'user_id' | 'torisan_id'
+    >,
+  ): Promise<TorisanStapleFoodType> {
+    return await this.prisma.getRlsClient(userId).torisan_staple_food.create({
+      data: {
+        torisan_id: torisanId,
+        staple_food_type: torisan.staple_food_type,
+        any_staple_food: torisan.any_staple_food,
+        any_other_foods: torisan.any_other_foods,
+        user_id: userId,
+      },
+    });
+  }
+
+  async batchCreateTorisanFoodOtherFoodType(
+    userId: string,
+    torisanId: number,
+    torisanStapleFoodId: number,
+    otherFoodTypes: number[],
+  ): Promise<number> {
+    const result = await this.prisma
+      .getRlsClient(userId)
+      .torisan_staple_food_other_food_type.createMany({
+        data: otherFoodTypes.map((otherFoodType) => {
+          return {
+            torisan_id: torisanId,
+            torisan_staple_food_id: torisanStapleFoodId,
+            other_food_type: otherFoodType,
+            user_id: userId,
+          };
+        }),
+      });
+    return result.count;
+  }
+}
--- server/src/torisans/torisans.service.ts
+++ server/src/torisans/torisans.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTorisan } from './models/createTorisan.model';
-import { PrismaService } from 'src/prisma.service';
 import { Torisan } from './models/torisan.model';
+import { TorisansRepository } from './torisans.repository';
 
 @Injectable()
 export class TorisansService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private repository: TorisansRepository) {}
 
   // async findAll(userId: string): Promise<Torisan[]> {
   //   const summaries = await this.prisma
@@ -28,55 +28,42 @@ export class TorisansService {
   async create(torisan: CreateTorisan, userId: string): Promise<Torisan> {
     const { objective, food } = torisan;
 
-    const createdBase = await this.prisma.getRlsClient(userId).torisan.create({
-      data: {
-        name: torisan.name,
-        nickname: torisan.nickname,
-        specie_type: torisan.specie_type,
-        birth_date: torisan.birth_date,
-        gender_type: torisan.gender_type,
-        stage_type: torisan.stage_type,
-        user_id: userId,
-      },
+    const createdBase = await this.repository.createTorisan(userId, {
+      name: torisan.name,
+      nickname: torisan.nickname,
+      specie_type: torisan.specie_type,
+      birth_date: torisan.birth_date,
+      gender_type: torisan.gender_type,
+      stage_type: torisan.stage_type,
     });
 
-    const createdObjective = await this.prisma
-      .getRlsClient(userId)
-      .torisan_objective.create({
-        data: {
-          torisan_id: createdBase.id,
-          body_weight: objective.body_weight,
-          amount_of_water: objective.amount_of_water,
-          amount_of_staple_food: objective.amount_of_staple_food,
-          user_id: userId,
-        },
-      });
+    const createdObjective = await this.repository.createTorisanObjective(
+      userId,
+      createdBase.id,
+      {
+        body_weight: objective.body_weight,
+        amount_of_water: objective.amount_of_water,
+        amount_of_staple_food: objective.amount_of_staple_food,
+      },
+    );
 
-    const createdFood = await this.prisma
-      .getRlsClient(userId)
-      .torisan_staple_food.create({
-        data: {
-          torisan_id: createdBase.id,
-          staple_food_type: food.staple_food_type,
-          any_staple_food: food.any_staple_food,
-          any_other_foods: food.any_other_foods,
-          user_id: userId,
-        },
-      });
+    const createdFood = await this.repository.createTorisanFood(
+      userId,
+      createdBase.id,
+      {
+        staple_food_type: food.staple_food_type,
+        any_staple_food: food.any_staple_food,
+        any_other_foods: food.any_other_foods,
+      },
+    );
 
     if (food.other_food_types.length > 0) {
-      await this.prisma
-        .getRlsClient(userId)
-        .torisan_staple_food_other_food_type.createMany({
-          data: food.other_food_types.map((otherFoodType) => {
-            return {
-              torisan_id: createdBase.id,
-              torisan_staple_food_id: createdFood.id,
-              other_food_type: otherFoodType,
-              user_id: userId,
-            };
-          }),
-        });
+      await this.repository.batchCreateTorisanFoodOtherFoodType(
+        userId,
+        createdBase.id,
+        createdFood.id,
+        food.other_food_types,
+      );
     }
 
     const {
