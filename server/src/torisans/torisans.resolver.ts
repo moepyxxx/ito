e56a9769b21f9dc1e14be63f1370549a346feb05
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-27 22:04:26 +0900

    add: ダミーのgraphQLを用意した

--- server/src/torisans/torisans.resolver.ts
+++ server/src/torisans/torisans.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Mutation, Args, Context, Query, Int } from '@nestjs/graphql';
 import { CreateTorisan } from './models/createTorisan.model';
 import { TorisansService } from './torisans.service';
-import { Torisan } from './models/torisan.model';
+import { Torisan, TorisanBase } from './models/torisan.model';
 import { ZodValidationPipe } from 'src/pipe/ZodValidationPipe';
 import { CreateTorisanZodInput } from './models/torisan.schema';
 import { Request } from 'express';
@@ -11,22 +11,66 @@ import { Request } from 'express';
 export class TorisansResolver {
   constructor(private readonly torisansService: TorisansService) {}
 
-  // Query root type must be provided.のエラーが出るためダミーで表示
-  @Query(() => [Int])
-  torisans(): number[] {
-    return [1, 2, 3];
+  @Query(() => [TorisanBase])
+  torisans(): TorisanBase[] {
+    return [
+      new TorisanBase({
+        id: 1,
+        name: 'うに',
+        nickname: 'うにちゃん',
+        specie_type: 1,
+        birth_date: new Date('2020-01-02 00:00:00'),
+        stage_type: 1,
+        gender_type: 1,
+      }),
+      new TorisanBase({
+        id: 1,
+        name: 'いと',
+        nickname: 'いとちゃん',
+        specie_type: 1,
+        birth_date: new Date('2020-01-02 00:00:00'),
+        stage_type: 1,
+        gender_type: 1,
+      }),
+      new TorisanBase({
+        id: 1,
+        name: 'たま',
+        nickname: 'たまちゃん',
+        specie_type: 1,
+        birth_date: new Date('2020-01-02 00:00:00'),
+        stage_type: 1,
+        gender_type: 1,
+      }),
+    ];
     // const userId = context.req.app['user_id'];
     // return this.torisansService.findAll(userId);
   }
 
-  // @Query(() => Torisan)
-  // torisan(
-  //   @Args('id') id: number,
-  //   @Context() context: { req: Request },
-  // ): Promise<Torisan> {
-  //   const userId = context.req.app['user_id'];
-  //   return this.torisansService.findDetailById(userId, id);
-  // }
+  @Query(() => Torisan)
+  torisan(@Args('id') id: number): Torisan {
+    return new Torisan({
+      id,
+      name: 'うに',
+      nickname: 'うにちゃん',
+      specie_type: 1,
+      birth_date: new Date('2020-01-02 00:00:00'),
+      stage_type: 1,
+      gender_type: 1,
+      objective: {
+        amount_of_water: 15,
+        amount_of_staple_food: 15,
+        body_weight: null,
+      },
+      food: {
+        staple_food_type: 1,
+        any_staple_food: null,
+        other_food_types: [1, 2, 3],
+        any_other_foods: null,
+      },
+    });
+    // const userId = context.req.app['user_id'];
+    // return this.torisansService.findDetailById(userId, id);
+  }
 
   @Mutation(() => Torisan)
   createTorisan(
