ab4d09160917d414b1d568b0a01b1f314b3f2624
moeko-iwai <moepyxxxxxx@gmail.com>
2024-03-27 22:35:42 +0900

    update: 認証状態を記録するようにした

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,7 +1,11 @@
+import { useCookies } from "react-cookie";
+
 /**
  * 認証状態を判定するhooks
  * TODO: 認証状態を判定してい返すようにする
  */
 export const useAuth = () => {
-  return false;
+  const [cookies] = useCookies(["access_token"]);
+
+  return !!cookies;
 };
