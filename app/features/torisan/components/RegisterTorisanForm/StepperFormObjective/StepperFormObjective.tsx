720ac52a1bd2ba3d9e6f60b8c7826fcd1bdf52bb
moeko-iwai <moepyxxxxxx@gmail.com>
2024-04-04 00:06:26 +0900

    refactor: separate objective form

--- app/features/torisan/components/RegisterTorisanForm/StepperFormObjective/StepperFormObjective.tsx
+++ app/features/torisan/components/RegisterTorisanForm/StepperFormObjective/StepperFormObjective.tsx
@@ -1,13 +1,13 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormTextBox } from "@/components/atoms/forms/FormTextBox";
 import { RenderStepActions } from "@/components/layouts/StepperLayout/useStepper";
-import { Note } from "@/components/molecules/Note";
 import {
   FormObjectiveEditType,
   FormObjectiveSubmitType,
+  getInitialObjectiveValue,
   objectiveSchema,
 } from "../../../schemas/objective";
+import { RHFFormObjective } from "../../RHF/RHFFormObjective";
 
 type Props = {
   renderStepperActions: RenderStepActions;
@@ -25,47 +25,14 @@ export const StepperFormObjective: React.FC<Props> = ({
     handleSubmit,
     formState: { errors },
   } = useForm<FormObjectiveEditType, any, FormObjectiveSubmitType>({
-    defaultValues:
-      initialValue != null
-        ? {
-            body_weight: initialValue.body_weight,
-            amount_of_staple_food: initialValue.amount_of_staple_food,
-            amount_of_water: initialValue.amount_of_water,
-          }
-        : {
-            body_weight: null,
-            amount_of_staple_food: null,
-            amount_of_water: null,
-          },
+    defaultValues: getInitialObjectiveValue(initialValue),
     mode: "onChange",
     resolver: zodResolver(objectiveSchema),
   });
 
   return (
     <div className="flex flex-col space-y-10">
-      <Note title="目標について">
-        itoは毎日の鳥さんの健康状態の”変化”に気づくことを目的としており、基本的には記録された体重・食べたご飯の量やお水の量に関して、毎日の変化を通知します。しかし、すでにお医者さんをはじめとした専門の方により目指すべき標準目標の約束がある場合、毎日の変化について標準情報と比べた結果をレポートとして作成します。基本的にすでにお医者さんにかかった鳥さんのための機能であり、この目標はあなたの鳥さんを直接見た専門の方からのアドバイスがある場合のみ設定することをおすすめしています。
-      </Note>
-      <FormTextBox
-        label="体重（g）"
-        inputType="number"
-        {...register("body_weight")}
-        errorMessage={errors.body_weight && errors.body_weight.message}
-      />
-      <FormTextBox
-        label="ごはんの量（g）"
-        inputType="number"
-        {...register("amount_of_staple_food")}
-        errorMessage={
-          errors.amount_of_staple_food && errors.amount_of_staple_food.message
-        }
-      />
-      <FormTextBox
-        label="お水の量（g）"
-        inputType="number"
-        {...register("amount_of_water")}
-        errorMessage={errors.amount_of_water && errors.amount_of_water.message}
-      />
+      <RHFFormObjective rhfRegister={register} rhfErrors={errors} />
       <div className="pt-2">
         {renderStepperActions(true, {
           onClickNext: (onNext: () => void) => {
--- app/features/torisan/schemas/objective.ts
+++ app/features/torisan/schemas/objective.ts
@@ -26,3 +26,18 @@ export type FormObjectiveEditType = {
   amount_of_staple_food: number | null;
   amount_of_water: number | null;
 };
+
+export const getInitialObjectiveValue = (
+  initialValue: FormObjectiveSubmitType | null
+) =>
+  initialValue != null
+    ? {
+        body_weight: initialValue.body_weight,
+        amount_of_staple_food: initialValue.amount_of_staple_food,
+        amount_of_water: initialValue.amount_of_water,
+      }
+    : {
+        body_weight: null,
+        amount_of_staple_food: null,
+        amount_of_water: null,
+      };
