61a7e70005ab0d2985cb5b33378b0985c06a61af
moeko-iwai <moepyxxxxxx@gmail.com>
2024-04-06 17:56:35 +0900

    add: EditTorisan model

--- server/src/torisans/models/torisan.schema.ts
+++ server/src/torisans/models/torisan.schema.ts
@@ -21,62 +21,52 @@ import {
 } from 'utils/getRequestErrorMessage';
 import { z } from 'zod';
 
-export const createTorisanSchema = z
+export const objectiveSchema = z
   .object({
-    name: z
+    body_weight: z
+      .number()
+      .nonnegative()
+      .max(50, getMaxIntError('目標体重', 50))
+      .nullable(),
+    amount_of_water: z
+      .number()
+      .nonnegative()
+      .max(50, getMaxIntError('目標の水の量', 50))
+      .nullable(),
+    amount_of_staple_food: z
+      .number()
+      .nonnegative()
+      .max(50, getMaxIntError('目標のごはんの量', 50))
+      .nullable(),
+  })
+  .required();
+
+export const foodSchema = z
+  .object({
+    staple_food_type: createCustomUnion<StapleFoodType>(
+      StapleFood,
+      '主食',
+      true,
+    ),
+    any_staple_food: z
       .string()
-      .min(1, getRequiredError('名前'))
-      .max(20, getMaxCharsError('名前', 20)),
-    nickname: z
+      .max(50, getMaxCharsError('主食（その他）', 50))
+      .nullable(),
+    other_food_types: z.array(
+      createCustomUnion<OtherFoodType>(OtherFood, '副食', true),
+    ),
+    any_other_foods: z
       .string()
-      .min(1, getRequiredError('ニックネーム'))
-      .max(25, getMaxCharsError('ニックネーム', 25)),
-    stage_type: createCustomUnion<StageType>(Stage, 'ステージ'),
-    specie_type: createCustomUnion<SpecieType>(Specie, '鳥さんの種類'),
-    birth_date: z.date().max(new Date(), getMaxDateIsTodayError('誕生日')),
-    gender_type: createCustomUnion<GenderType>(Gender, '性別'),
-    objective: z
-      .object({
-        body_weight: z
-          .number()
-          .nonnegative()
-          .max(50, getMaxIntError('目標体重', 50))
-          .nullable(),
-        amount_of_water: z
-          .number()
-          .nonnegative()
-          .max(50, getMaxIntError('目標の水の量', 50))
-          .nullable(),
-        amount_of_staple_food: z
-          .number()
-          .nonnegative()
-          .max(50, getMaxIntError('目標のごはんの量', 50))
-          .nullable(),
-      })
-      .required(),
-    food: z
-      .object({
-        staple_food_type: createCustomUnion<StapleFoodType>(StapleFood, '主食'),
-        any_staple_food: z
-          .string()
-          .max(50, getMaxCharsError('主食（その他）', 50))
-          .nullable(),
-        other_food_types: z.array(
-          createCustomUnion<OtherFoodType>(OtherFood, '副食'),
-        ),
-        any_other_foods: z
-          .string()
-          .max(50, getMaxCharsError('副食（その他）', 50))
-          .nullable(),
-      })
-      .required(),
+      .max(50, getMaxCharsError('副食（その他）', 50))
+      .nullable(),
   })
   .required()
   .superRefine((data, ctx) => {
     if (
-      data.food.staple_food_type === StapleFoodAnySelect &&
-      !data.food.any_staple_food
+      data.staple_food_type === StapleFoodAnySelect &&
+      !data.any_staple_food
     ) {
+      console.log('ここ通る？');
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         path: ['any_staple_food'],
@@ -87,8 +77,8 @@ export const createTorisanSchema = z
   })
   .superRefine((data, ctx) => {
     if (
-      data.food.other_food_types.includes(OtherFoodsAnySelect) &&
-      !data.food.any_other_foods
+      data.other_food_types.includes(OtherFoodsAnySelect) &&
+      !data.any_other_foods
     ) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
@@ -98,5 +88,24 @@ export const createTorisanSchema = z
     }
     return true;
   });
+
+export const createTorisanSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, getRequiredError('名前'))
+      .max(20, getMaxCharsError('名前', 20)),
+    nickname: z
+      .string()
+      .min(1, getRequiredError('ニックネーム'))
+      .max(25, getMaxCharsError('ニックネーム', 25)),
+    stage_type: createCustomUnion<StageType>(Stage, 'ステージ', true),
+    specie_type: createCustomUnion<SpecieType>(Specie, '鳥さんの種類', true),
+    birth_date: z.date().max(new Date(), getMaxDateIsTodayError('誕生日')),
+    gender_type: createCustomUnion<GenderType>(Gender, '性別', true),
+    objective: objectiveSchema,
+    food: foodSchema,
+  })
+  .required();
 
 export type CreateTorisanZodInput = z.infer<typeof createTorisanSchema>;
--- server/utils/createCustomUnion.ts
+++ server/utils/createCustomUnion.ts
@@ -1,13 +1,21 @@
-import { getConstantsExceptionError } from './getRequestErrorMessage';
+import {
+  getConstantsExceptionError,
+  getRequiredError,
+} from './getRequestErrorMessage';
 import { z } from 'zod';
 
-export const createCustomUnion = <T extends number>(
+export const createCustomUnion = <T extends number | null>(
   object: Record<string, T>,
   label: string,
+  required?: boolean,
 ) => {
-  return z.custom<T>(
-    (data) =>
-      typeof data === 'number' && Object.values(object).includes(data as T),
-    getConstantsExceptionError(label),
-  );
+  return z.custom<T>((data) => {
+    if (data === null && required) return getRequiredError(label);
+    if (data === null) {
+      return true;
+    }
+    return (
+      typeof data === 'number' && Object.values(object).includes(data as T)
+    );
+  }, getConstantsExceptionError(label));
 };
